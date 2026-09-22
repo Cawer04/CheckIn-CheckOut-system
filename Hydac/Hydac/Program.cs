348aaf85311f75dfc5b3e549e7e5dbdd691dacb1
Cawer04 <cawer768@gmail.com>
2026-09-22 15:01:04 +0200

    Sej kode

--- Hydac/Hydac/Program.cs
+++ Hydac/Hydac/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Virker det her?");
+            Console.WriteLine("Team A2 for the win! vi er mega gode <3");
         }
     }
 }

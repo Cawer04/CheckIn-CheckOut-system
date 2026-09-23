073ca7f163754151ce0799b7cd01cb43b6f211ac
Cawer04 <cawer768@gmail.com>
2026-09-23 12:25:34 +0200

    har skrevet hello world

--- Hydac/Hydac/Program.cs
+++ Hydac/Hydac/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Team A2 for the win! vi er mega gode <3");
+            Console.WriteLine("hello world!");
         }
     }
 }
